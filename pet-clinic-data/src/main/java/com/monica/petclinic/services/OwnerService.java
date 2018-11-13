841dee9c50998d93825c8feebab28f1ec4c5706d
Khasanboy <akbarov8808@gmail.com>
2018-11-13 21:28:02 +0200

    Service interfaces are created

---
+++ pet-clinic-data/src/main/java/com/monica/petclinic/services/OwnerService.java
@@ -0,0 +1,18 @@
+package com.monica.petclinic.services;
+
+import com.monica.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+
+}
