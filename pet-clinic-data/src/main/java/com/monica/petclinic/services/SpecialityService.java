27488fed099f0de5420566b714cf7ef96aa1f581
Khasanboy <akbarov8808@gmail.com>
2018-12-09 12:16:04 +0200

    Speciality service is implemented

---
+++ pet-clinic-data/src/main/java/com/monica/petclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.monica.petclinic.services;
+
+import com.monica.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+
+
+}
--- pet-clinic-web/src/main/java/com/monica/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/monica/petclinic/bootstrap/DataLoader.java
@@ -2,11 +2,16 @@ package com.monica.petclinic.bootstrap;
 
 
 import com.monica.petclinic.model.Owner;
+import com.monica.petclinic.model.Pet;
+import com.monica.petclinic.model.PetType;
 import com.monica.petclinic.model.Vet;
 import com.monica.petclinic.services.OwnerService;
+import com.monica.petclinic.services.PetTypeService;
 import com.monica.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -14,25 +19,57 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     //private final PetService petService;
+    private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType =  petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Bricket");
+        owner1.setCity("New York");
+        owner1.setTelephone("5664748393");
+
+        Pet michelsPet = new Pet();
+        michelsPet.setPetType(savedDogPetType);
+        michelsPet.setOwner(owner1);
+        michelsPet.setBirthDate(LocalDate.now());
+        michelsPet.setName("Rosco");
+        owner1.getPets().add(michelsPet);
+
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glinamer");
+        owner2.setAddress("345 Linu");
+        owner2.setCity("London");
+        owner2.setTelephone("345677");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(savedCatPetType);
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setName("Monica");
+        owner2.getPets().add(fionasCat);
+
 
         ownerService.save(owner2);
         System.out.println("Owners are loaded");
